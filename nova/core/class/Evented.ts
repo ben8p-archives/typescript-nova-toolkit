4354c8aea58483ba88b6db9b78e1589d61385a36
Benjamin Santalucia <ben@dojotoolkit-fr.org>
2015-04-09 13:42:31 +0200

    add evented class

---
+++ nova/core/class/Evented.ts
@@ -0,0 +1,49 @@
+/**
+ * represent an evented class
+ * implements EventTarget interface
+ */
+class Evented implements EventTarget {
+	private events:any = {};
+	/**
+	 * detach a handler from an event type in the event bus
+	 * @param	type		the type event to attach
+	 * @param	listener	the handler to attach
+	 */
+	removeEventListener(type:string, listener:Function) {
+		var handlers = <Function[]>this.events[type];
+		handlers.some((handler, index) => {
+			if(handler === listener) {
+				handlers[index] = null;
+				return true;
+			}
+			return false;
+		});
+	}
+	/**
+	 * attach a handler to an event type in the event bus
+	 * @param	type		the type event to attach
+	 * @param	listener	the handler to attach
+	 */
+	addEventListener(type:string, listener:Function) {
+		this.events[type] = this.events[type] || [];
+		this.events[type].push(listener);
+	}
+	/**
+	 * dispatch a event to the event bus
+	 * does not support cancelable/bubbles/preventDefault()/stopPropagation()
+	 * @param	event	the event to dispatch
+	 * @return	boolean
+	 */
+	dispatchEvent(event:Event):boolean {
+		var handlers = <Function[]>this.events[event.type];
+		if(!handlers || handlers.length === 0) { return true; }
+		handlers.forEach((handler) => {
+			if(typeof handler === 'function') {
+				handler(event);
+			}
+		});
+		return true;
+	}
+}
+
+export = Evented;
--- nova/core/event.ts
+++ nova/core/event.ts
@@ -1,3 +1,5 @@
+import Evented = require('./class/Evented');
+
 /** interface for events returned when() and once() */
 interface IRootEvent {
 	then(successCallback:Function):ISubEvent;
@@ -82,54 +84,9 @@ class InternalEvent {
 	}
 }
 
-/**
- * represent an event bus
- * implementation of publish/subscribe mechanism
- */
-class EventBus implements EventTarget {
-	private events:any = {};
-	/**
-	 * detach a handler from an event type in the event bus
-	 * @param	type		the type event to attach
-	 * @param	listener	the handler to attach
-	 */
-	removeEventListener(type:string, listener:Function) {
-		var handlers = <Function[]>this.events[type];
-		handlers.some((handler, index) => {
-			if(handler === listener) {
-				handlers[index] = null;
-				return true;
-			}
-			return false;
-		});
-	}
-	/**
-	 * attach a handler to an event type in the event bus
-	 * @param	type		the type event to attach
-	 * @param	listener	the handler to attach
-	 */
-	addEventListener(type:string, listener:Function) {
-		this.events[type] = this.events[type] || [];
-		this.events[type].push(listener);
-	}
-	/**
-	 * dispatch a event to the event bus
-	 * does not support cancelable/bubbles/preventDefault()/stopPropagation()
-	 * @param	event	the event to dispatch
-	 * @return	boolean
-	 */
-	dispatchEvent(event:Event):boolean {
-		var handlers = <Function[]>this.events[event.type];
-		if(!handlers || handlers.length === 0) { return true; }
-		handlers.forEach((handler) => {
-			if(typeof handler === 'function') {
-				handler(event);
-			}
-		});
-		return true;
-	}
-}
-let eventBus = new EventBus();
+
+/** implementation of the bus for publish/subscribe pattern */
+let eventBus = new Evented();
 
 
 /**
