545e5da021787d45139a26d811a773c769aa3a62
ben <ben@woow-fr.com>
2015-04-19 20:56:38 +0200

    mess up with the update

--- nova/core/AMDPlugin.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Interface for writing an AMD plugin
- */
-interface AMDPlugin {
-	load?: (moduleName: string, parentRequire: Function, onLoad: (value?: any) => void, config?: Object) => void;
-	normalize?: (moduleName: string, normalize: (moduleName: string) => string) => string;
-}
-export = AMDPlugin;
