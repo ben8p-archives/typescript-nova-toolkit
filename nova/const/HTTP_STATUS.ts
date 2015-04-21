938281ba860f504ab89777798286bb6f22e93421
Benjamin Santalucia <ben@dojotoolkit-fr.org>
2015-04-21 13:48:08 +0200

    add http status constant

--- nova/const/HTTP_STATUS.ts
+++ nova/const/HTTP_STATUS.ts
@@ -1,4 +1,4 @@
-var status: {[key: string]: number} = {
+var STATUSES: {[key: string]: number} = {
 	// This means that the server has received the request headers, and that the client should proceed to send the request body (in the case of a request for which a body needs to be sent; for example, a POST request). If the request body is large, sending it to a server when a request has already been rejected based upon inappropriate headers is inefficient. To have a server check if the request could be accepted based on the request's headers alone, a client must send Expect: 100-continue as a header in its initial request and check if a 100 Continue status code is received in response before continuing (or receive 417 Expectation Failed and not continue).
 	CONTINUE: 100,
 	// This means the requester has asked the server to switch protocols and the server is acknowledging that it will do so.
@@ -188,4 +188,4 @@ var status: {[key: string]: number} = {
 	// (Unknown) This status code is not specified in any RFCs, but is used by Microsoft HTTP proxies to signal a network connect timeout behind the proxy to a client in front of the proxy.[citation needed]
 	NETWORK_CONNECT_TIMEOUT_ERROR: 599
 };
-export = status;
+export = STATUSES;
