120486a11f79ecf8873344508eaefd5ad9836e65
Benjamin Santalucia <ben@dojotoolkit-fr.org>
2015-04-21 12:57:47 +0200

    add debounce

---
+++ nova/core/debounce.ts
@@ -0,0 +1,18 @@
+/**
+ * Create a function that will only execute after `wait` milliseconds
+ * of repeated execution. Useful for delaying some event action slightly to allow
+ * for rapidly-firing events such as window.resize, node.mousemove and so on.
+ */
+function debounce(callback: Function, wait? : number): Function {
+	var timer: number;
+	wait = wait || 100;
+	return function() {
+		if (timer) {clearTimeout(timer); }
+		var currentArguments = arguments;
+
+		timer = setTimeout(function() {
+			callback.apply(callback, currentArguments);
+		}, wait);
+	};
+};
+export = debounce;
