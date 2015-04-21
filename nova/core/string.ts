307f190a0ea3f2185e3fb02fe863010dc4855f45
Benjamin Santalucia <ben@dojotoolkit-fr.org>
2015-04-21 13:44:16 +0200

    add string utils

--- nova/core/string.ts
+++ nova/core/string.ts
@@ -1,5 +1,20 @@
-const templateToken = /\$\{([a-z0-9_-]+)\}/gi;
-
+const TEMPLATE_TOKEN = /\$\{([a-z0-9_-]+)\}/gi;
+const ESCAPE_CODE_POINTS: {[index: number]: any} = {
+	0: false, // nul; most browsers truncate because they use c strings under the covers.
+	10: '\n', // new line
+	13: '\r', // carriage return
+	34: '"', // double quote
+	38: '&', // ampersant
+	39: '\'', // single quote
+	60: '<', // less than
+	62: '>', // greater than
+	92: '\\', // Backslash
+	8232: true, // line separator
+	8233: true // paragraph separator
+};
+const ESCAPED_REGEXP = /&#[0-9]+;/g;
+const ESCAPED_NOISE = /&|#|;/g;
+const CAMEL_CASE_REGEXP = /^.|-.| ./g;
 /**
  * replace token ${} in a string
  * @param	text	the string to update
@@ -10,5 +25,68 @@ export function interpolate(text: string, content: {[index: string]: any}): stri
 	let substitute = function (placeHolder: string, attribute: string): string {
 		return content[attribute] || placeHolder;
 	};
-	return text.replace(templateToken, substitute);
+	return text.replace(TEMPLATE_TOKEN, substitute);
+}
+
+/** escape a string */
+export function escape(value: string): string {
+	if (!value) {
+		return value;
+	}
+	var out: string[] = [],
+		charCode: number,
+		shouldEscape: boolean,
+		j = value.length;
+	for (let i = 0; i < j; ++i) {
+		charCode = value.charCodeAt(i);
+		shouldEscape = ESCAPE_CODE_POINTS[charCode];
+		if (shouldEscape === undefined) {
+			// undefined or null are OK.
+			out.push(value.charAt(i));
+		} else if (shouldEscape) {
+			out.push('&#', charCode.toString(), ';');
+		} //else we remove the char
+	}
+	return out.join('');
+}
+
+/** unescape a string */
+export function unescape(value: string): string {
+	if (!value) {
+		return value;
+	}
+	var itm: string[] = value.match(ESCAPED_REGEXP) || [],
+		len = itm.length;
+	for (let i = 0; i < len; i++) {
+		let newValue = ESCAPE_CODE_POINTS[+itm[i].replace(ESCAPED_NOISE, '')];
+		if (typeof newValue === 'string') {
+			value = value.replace(itm[i], newValue);
+		}
+	}
+	return value;
+}
+/** convert to camel case */
+export function toCamelCase(value: string, capitalizeFirstChar?: boolean): string {
+	return value.replace(CAMEL_CASE_REGEXP, function(letter: string, index: number): string {
+		return index === 0 ? (capitalizeFirstChar ? letter.toUpperCase() : letter.toLowerCase()) : letter.substring(1).toUpperCase();
+	});
+}
+/** escape a string to be used in a regexp */
+export function escapeForRegExp(value: string) {
+	return value
+		.replace(/\\/g, '\\\\') // the order here is very important!!
+		.replace(/\//g, '\\/') // the order here is very important!!
+		.replace(/\+/g, '\\+')
+		.replace(/\[/g, '\\[')
+		.replace(/\]/g, '\\]')
+		.replace(/\./g, '\\.')
+		.replace(/\(/g, '\\(')
+		.replace(/\)/g, '\\)')
+		.replace(/\*/g, '\\*')
+		.replace(/\?/g, '\\?')
+		.replace(/\|/g, '\\|')
+		.replace(/\$/g, '\\$')
+		.replace(/\{/g, '\\{')
+		.replace(/\}/g, '\\}')
+		.replace(/\^/g, '\\^');
 }
--- tests/core/string.ts
+++ tests/core/string.ts
@@ -22,6 +22,32 @@ registerSuite(function () {
 				world: 'BAR'
 			});
 			assert.equal(newString, 'hello BAR ! Current date is ${now}');
+		},
+		'String escape': function() {
+			var testString = '<',
+				targetString = '&#60;';
+			assert.equal(stringUtil.escape(testString), targetString, 'The string should have been escaped properly');
+		},
+
+		'String unescape': function() {
+			var testString = '&#60;',
+				targetString = '<';
+			assert.equal(stringUtil.unescape(testString), targetString, 'The string should have been unescaped properly');
+		},
+
+		'String camelCase': function() {
+			var testString = 'a-Test String',
+				targetString = 'aTestString';
+
+			assert.equal(stringUtil.toCamelCase(testString), targetString, 'The string has not been converted to camelcase properly');
+
+		},
+		'String regExp escape': function() {
+			var testString = 'this $ is ^ a [string (to) escape]',
+				targetString = 'this \\$ is \\^ a \\[string \\(to\\) escape\\]';
+
+			assert.equal(stringUtil.escapeForRegExp(testString), targetString, 'The string has not been escaped properly');
+
 		}
 	};
 });
