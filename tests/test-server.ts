005111d31098099691bb4be25a85f381845c0f7c
Benjamin Santalucia <ben@dojotoolkit-fr.org>
2015-04-22 10:39:13 +0200

    typo

--- tests/test-server.ts
+++ tests/test-server.ts
@@ -14,7 +14,7 @@ app.all('*', function(req: any, res: any){
 
 var server = app.listen(3000, function () {
 	var port = server.address().port;
-	console.log('Test are available here :');
+	console.log('Tests are available here :');
 	console.log('http://localhost:%s/node_modules/intern/client.html?config=tests/intern', port);
 });
 module.exports = server;
