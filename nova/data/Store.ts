8ea8bc6faa8b0ae04ccdf8f4b16d6b1a8879a9db
Benjamin Santalucia <ben@dojotoolkit-fr.org>
2015-06-19 11:18:11 +0200

    store.query is now Observable

--- nova/data/Store.ts
+++ nova/data/Store.ts
@@ -1,4 +1,6 @@
 import Stateful = require('../class/Stateful');
+import Evented = require('../class/Evented');
+import CustomEvent = require('../class/CustomEvent');
 import object = require('../core/object');
 
 function match(query: any, item: {[key: string]: any; }, key: string): boolean {
@@ -13,41 +15,97 @@ function match(query: any, item: {[key: string]: any; }, key: string): boolean {
 	return false;
 }
 
-/** store for data managment */
+/** interface for events */
+interface QueryRootEvent {
+	then(queryCallback: (items: {[key: string]: any; }[]) => void): QuerySubEvent;
+}
+/** interface for events returned by then() */
+interface QuerySubEvent {
+	then(queryCallback: (items: {[key: string]: any; }[]) => void): QuerySubEvent;
+	remove(): void;
+}
+
+/**
+ * store for data managment.
+ * .query() return an observable item
+ */
 class Store extends Stateful {
 	data: {[key: string]: any; }[]; //for interface
 	private _data: {[key: string]: any; }[] = null; //real data handler
 
 	protected setData(data: {[key: string]: any; }[]): void {
 		this._data = data;
+		this._dispatchEvent();
 	}
 	protected getData(): {[key: string]: any; }[] {
 		return this._data;
 	}
-	sort(compareFunction?: (itemA: {[key: string]: any; }, itemB: {[key: string]: any; }) => number): {[key: string]: any}[] {
-		compareFunction = compareFunction || function(itemA: {[key: string]: any; }, itemB: {[key: string]: any; }): number {
-			var jsonA = JSON.stringify(itemA);
-			var jsonB = JSON.stringify(itemB);
 
-			return (jsonA > jsonB)
-				? 1
-				: (jsonA < jsonB)
-					? -1
-					: 0;
-		};
+	private _eventCache: Evented[] = [];
 
-		return this._data.sort(compareFunction);
-	}
-	query(query: {[key: string]: string|number|boolean|RegExp|((value: any, key: string, item: {[key: string]: any; }) => boolean); }): {[key: string]: any; }[] {
-		return this._data.filter((item: {[key: string]: any; }) => {
-			var result: boolean = true;
-			object.forEach(query, function(value: any, key: string): void {
-				if (!item.hasOwnProperty(key) || !match(value, item, key)) {
-					result = false;
-				}
-			});
-			return result;
+	/** update all queries */
+	private _dispatchEvent() {
+		this._eventCache.forEach((eventBus) => {
+			eventBus.dispatchEvent(new CustomEvent('update'));
 		});
+	}
+
+	/** add data to the store */
+	add(item: {[key: string]: any; }): {[key: string]: any} {
+		if (this._data.indexOf(item) === -1) {
+			this._data.push(item);
+			this._dispatchEvent();
+			return item;
+		}
+		return null;
+	}
+	/** remove data from the store */
+	remove(item: {[key: string]: any; }): {[key: string]: any} {
+		var index = this._data.indexOf(item);
+		if (index >= 0) {
+			this._data.splice(index, 1);
+			this._dispatchEvent();
+			return item;
+		}
+		return null;
+	}
+
+	/** filter the data using the query, sort them if requested */
+	query(query?: {[key: string]: string|number|boolean|RegExp|((value: any, key: string, item: {[key: string]: any; }) => boolean); },
+			compareFunction?: (itemA: {[key: string]: any; }, itemB: {[key: string]: any; }) => number): QueryRootEvent {
+
+		//each query is Observable, so each query has its own event bus
+		var eventBus = new Evented();
+		var then = (queryCallback: (items: {[key: string]: any; }[]) => void): QuerySubEvent => {
+			//caling then, actually add an event listener
+			var listener = (evt: Event): void => {
+				//this event listener, will run and return the query
+				var filter = this._data.filter((item: {[key: string]: any; }) => {
+					var result: boolean = true;
+					object.forEach(query, function(value: any, key: string): void {
+						if (!item.hasOwnProperty(key) || !match(value, item, key)) {
+							result = false;
+						}
+					});
+					return result;
+				});
+				queryCallback(compareFunction ? filter.sort(compareFunction) : filter);
+			};
+			eventBus.addEventListener('update', listener);
+
+			//execute the callback immediatly
+			listener(null);
+
+			return {
+				then: then,
+				remove: function() { eventBus.removeEventListener('update', listener); }
+			};
+		};
+		this._eventCache.push(eventBus);
+
+		return <QueryRootEvent> {
+			then: then
+		};
 	}
 }
 export = Store;
--- tests/data/Store.ts
+++ tests/data/Store.ts
@@ -38,11 +38,11 @@ registerSuite(function () {
 				];
 			},
 			'store': {
-				'.sort': function() {
+				'sort': function() {
 					var store = new Store();
 					store.data = data;
 
-					store.sort(function(itemA: {[key: string]: any; }, itemB: {[key: string]: any; }): number {
+					store.query(null, function(itemA: {[key: string]: any; }, itemB: {[key: string]: any; }): number {
 						if (itemA['c'] > itemB['c']) {
 							return 1;
 						}
@@ -50,58 +50,174 @@ registerSuite(function () {
 							return -1;
 						}
 						return 0;
+					}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result[0]['c'], 'bar');
+						assert.equal(result[1]['c'], 'barbaz');
+						assert.equal(result[2]['c'], 'baz');
+						assert.equal(result[3]['c'], 'foo');
+						assert.equal(result[4]['c'], 'foobar');
 					});
-					assert.equal(store.data[0]['c'], 'bar');
-					assert.equal(store.data[1]['c'], 'barbaz');
-					assert.equal(store.data[2]['c'], 'baz');
-					assert.equal(store.data[3]['c'], 'foo');
-					assert.equal(store.data[4]['c'], 'foobar');
 
 				},
 				'.query': function() {
 					var store = new Store();
 					store.data = data;
 
-					var result: {[key: string]: any; }[];
+					store.query({c: 'baz'}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result[0]['c'], 'baz');
+						assert.equal(result.length, 1);
+					});
 
-					result = store.query({c: 'baz'});
-					assert.equal(result[0]['c'], 'baz');
-					assert.equal(result.length, 1);
+					store.query({a: 3}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result[0]['c'], 'baz');
+						assert.equal(result.length, 1);
+					});
 
-					result = store.query({a: 3});
-					assert.equal(result[0]['c'], 'baz');
-					assert.equal(result.length, 1);
+					store.query({b: true}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result[0]['c'], 'foo');
+						assert.equal(result[1]['c'], 'baz');
+						assert.equal(result[2]['c'], 'barbaz');
+						assert.equal(result.length, 3);
+					});
 
-					result = store.query({b: true});
-					assert.equal(result[0]['c'], 'foo');
-					assert.equal(result[1]['c'], 'baz');
-					assert.equal(result[2]['c'], 'barbaz');
-					assert.equal(result.length, 3);
+					store.query({c: /^ba/}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result[0]['c'], 'bar');
+						assert.equal(result[1]['c'], 'baz');
+						assert.equal(result[2]['c'], 'barbaz');
+						assert.equal(result.length, 3);
+					});
 
-					result = store.query({c: /^ba/});
-					assert.equal(result[0]['c'], 'bar');
-					assert.equal(result[1]['c'], 'baz');
-					assert.equal(result[2]['c'], 'barbaz');
-					assert.equal(result.length, 3);
+					store.query({b: true, c: /^ba/}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result[0]['c'], 'baz');
+						assert.equal(result[1]['c'], 'barbaz');
+						assert.equal(result.length, 2);
+					});
 
-					result = store.query({b: true, c: /^ba/});
-					assert.equal(result[0]['c'], 'baz');
-					assert.equal(result[1]['c'], 'barbaz');
-					assert.equal(result.length, 2);
+					store.query({b: true, g: true}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result.length, 0);
+					});
 
-					result = store.query({b: true, g: true});
-					assert.equal(result.length, 0);
+					store.query({g: true}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result.length, 0);
+					});
 
-					result = store.query({g: true});
-					assert.equal(result.length, 0);
-
-					result = store.query({a: function(value: any, key: string, item: {[key: string]: any; }): boolean {
+					store.query({a: function(value: any, key: string, item: {[key: string]: any; }): boolean {
 						return value % 2 === 0;
-					}});
-					assert.equal(result[0]['c'], 'bar');
-					assert.equal(result[1]['c'], 'foobar');
-					assert.equal(result.length, 2);
+					}}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result[0]['c'], 'bar');
+						assert.equal(result[1]['c'], 'foobar');
+						assert.equal(result.length, 2);
+					});
 
+				},
+				'chain': function() {
+					var store = new Store();
+					var count = 0;
+					store.data = data;
+
+					store.query({c: 'baz'}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result[0]['c'], 'baz');
+						count += 2;
+						assert.equal(count, 2);
+					}).then(function(result: {[key: string]: any}[]) {
+						assert.equal(result.length, 1);
+						count += 9;
+						assert.equal(count, 11);
+					});
+				},
+				'add/remove': function() {
+					var store = new Store();
+					store.data = data;
+					assert.isNotNull(store.add({
+						a: 10,
+						b: true,
+						c: 'barbaz'
+					})); //new object so it is added
+					assert.isNotNull(store.add({
+						a: 11,
+						b: true,
+						c: 'barbaz'
+					})); //new object so it is added
+					assert.isNull(store.add(data[0])); //will not be added
+					assert.isNotNull(store.remove(data[0])); //will be removed
+					assert.isNull(store.remove({
+						a: 11,
+						b: true,
+						c: 'barbaz'
+					})); //will NOT be removed (because it is not a pointer)
+
+					store.query().then(function(result: {[key: string]: any}[]) {
+						assert.equal(result.length, 6);
+					});
+				},
+				'observable query': function() {
+					var store = new Store();
+					var countAll = 0;
+					var countRemoval = 0;
+					var countSubset = 0;
+					var expectedSubset = [1, 2, 2, 3];
+					var resultSubset: number[] = [];
+					var finalSorted: any;
+					store.data = data;
+
+					//all data
+					store.query().then(function(result: {[key: string]: any}[]) {
+						assert.equal(data.length, result.length);
+						countAll++;
+					});
+
+					//only barbaz data
+					store.query({c: 'barbaz'}).then(function(result: {[key: string]: any}[]) {
+						countSubset++;
+						resultSubset.push(result.length);
+					});
+
+					//only barbaz data, ordered on value of "a" (descending)
+					store.query({c: 'barbaz'}, function(itemA: {[key: string]: any; }, itemB: {[key: string]: any; }): number {
+						if (itemA['a'] > itemB['a']) {
+							return -1;
+						}
+						if (itemA['a'] < itemB['a']) {
+							return 1;
+						}
+						return 0;
+					}).then(function(result: {[key: string]: any}[]) {
+						finalSorted = result;
+					});
+
+					store.query().then(function(result: {[key: string]: any}[]) {
+						countRemoval++;
+					}).remove();
+
+					//triger some updates
+					store.add({
+						a: 10,
+						b: true,
+						c: 'barbaz'
+					});
+					store.add({
+						a: 8,
+						b: true,
+						c: 'babaz'
+					});
+					store.add({
+						a: 9,
+						b: true,
+						c: 'barbaz'
+					});
+
+					//check that filtered query are returning the right subset
+					assert.deepEqual(expectedSubset, resultSubset);
+
+					//check that filtered query are returning the right subset in the right order
+					assert.deepEqual(finalSorted[0].a, 10);
+
+					//check that propagation is working in both "all" and "filtered" state
+					assert.equal(countSubset, 4);
+					assert.equal(countAll, 4);
+
+					//check that removed query are not re-executed
+					assert.equal(countRemoval, 1);
 				}
 			}
 		};
